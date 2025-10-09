9a4baebb8d5745a8d907fd11227cbe837f316088
mike <miketriticum@gmail.com>
2025-10-09 11:00:33 +0200

    20251009001 taServer missing

--- taApp.js
+++ taApp.js
@@ -1,74 +1,76 @@
+/*
+ Main javascript module
 
-async function getList( filter ) {
-
-
-  const buffer = await fetch( '/tsv/taCorpEthics' );
+*/
+async function getList(filter) {
+  const buffer = await fetch("/tsv/taCorpEthics");
 
   const resp = await buffer.text();
 
   const GROUP_KEY = 8;
 
-  let tsvA = new TabSeparatedValuesIntoArray( resp, GROUP_KEY, filter, [ 6, 1, 12, 13, 14, 17, 18, 16 ] );
+  let tsvA = new TabSeparatedValuesIntoArray(
+    resp,
+    GROUP_KEY,
+    filter,
+    [6, 1, 12, 13, 14, 17, 18, 16]
+  );
 
   //document.getElementById( 'a1' ).appendChild( tsvA.tsvCR( [ 1, 2, 5, 6, 8, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21 ] ) );
   //document.getElementById( 'a1' ).appendChild( tsvA.tsvCR( [ 6, 1, 2, 5, 8, 11, 12, 13, 14, 17, 18, 16, 19, 20] ) );
   // document.getElementById( 'a1' ).appendChild( tsvA.tsvSumma( 20 ) );
 
-
-  document.getElementById( 'b1' ).innerHTML = tsvA.tsv2fixed();
-
-
+  document.getElementById("b1").innerHTML = tsvA.tsv2fixed();
 }
 async function IBM() {
-
-  const buffer = await fetch( '/IBM/Manuals' );
+  const buffer = await fetch("/IBM/Manuals");
 
   const tsv = await buffer.text();
 
-  localStorage.setItem( "original", tsv )
+  localStorage.setItem("original", tsv);
 
   const GROUP_KEY = 1;
 
-  let tsvA = new TabSeparatedValuesIntoArray( tsv, GROUP_KEY );
+  let tsvA = new TabSeparatedValuesIntoArray(tsv, GROUP_KEY);
 
-  a1.appendChild( tsvA.tsvRC() );
+  a1.appendChild(tsvA.tsvRC());
 }
 
-
 async function JPY() {
-
-  const buffer = await fetch( '/SYMBOLS' );
+  const buffer = await fetch("/SYMBOLS");
 
   const tsv = await buffer.text();
 
-  const All = JSON.parse( tsv );
+  const All = JSON.parse(tsv);
 
-  let tsvA = new TabSeparatedValuesIntoArray( tsv, GROUP_KEY );
+  let tsvA = new TabSeparatedValuesIntoArray(tsv, GROUP_KEY);
 
-  document.getElementById( 'a1' ).appendChild( tsvA.tsvRC() );
+  document.getElementById("a1").appendChild(tsvA.tsvRC());
 }
-
 
 function post() {
   /*
-  *  loop thru table rows, then cells th, td
-  *  using same sequence as tab separated values when created
-  */
-  let tsv = []
-  var tr_rows = document.querySelectorAll( 'tr' );
-  for ( elements of tr_rows ) {
-    let td_th_cells = elements.children
-    let line = []
-    for ( element of td_th_cells ) {
-      line.push( element.textContent )
+   *  loop thru table rows, then cells th, td
+   *  using same sequence as tab separated values when created
+   */
+  let tsv = [];
+  var tr_rows = document.querySelectorAll("tr");
+  for (elements of tr_rows) {
+    let td_th_cells = elements.children;
+    let line = [];
+    for (element of td_th_cells) {
+      line.push(element.textContent);
     }
-    tsv.push( line.join( '\t' ) )
+    tsv.push(line.join("\t"));
   }
 
-  localStorage.setItem( "tsv", tsv.join( '\r\n' ) )
+  localStorage.setItem("tsv", tsv.join("\r\n"));
 }
 
-a1.addEventListener( ( 'blur' ), function ( event ) { post() } )
+a1.addEventListener("blur", function (event) {
+  post();
+});
 // getList(new URL(window.location.href).searchParams.get('filter'));
-document.cookie = "username=JohnDoe; expires=Thu, 31 Dec 2029 23:59:59 GMT; path=/";
+document.cookie =
+  "username=JohnDoe; expires=Thu, 31 Dec 2029 23:59:59 GMT; path=/";
 getList();
--- taServer.js
+++ taServer.js
@@ -1,164 +1,161 @@
-'use strict';
-const { Storage } = require( '@google-cloud/storage' );
-const https = require( 'node:http' );
-const fs = require( 'node:fs' );
-const path = require( 'node:path' );
-const process = require( 'node:process' );
-const stream = require( 'node:stream' );
-const symbolData = require( './taSymbolPeriod.js' );
-const myname = 'taServer.js';
+"use strict";
+const { Storage } = require("@google-cloud/storage");
+const https = require("node:http");
+const fs = require("node:fs");
+const path = require("node:path");
+const process = require("node:process");
+const stream = require("node:stream");
+const symbolData = require("./taSymbolPeriod.js");
+const myname = "taServer.js";
 var pileUp = [];
-var argGet = '';
-var reqURL = '';
+var argGet = "";
+var reqURL = "";
 /*
-*
-*/
-https.createServer().on( 'request', main ).listen( 3000 );
+ *
+ */
+https.createServer().on("request", main).listen(3000);
 
 /*
-* Main is just declarations and some global vars, which is needed due to the event driven nature
-*/
-async function main ( request, response ) {
+ * Main is just declarations and some global vars, which is needed due to the event driven nature
+ */
+async function main(request, response) {
+  request.on("data", collect_data.bind(this));
+  request.on("end", process_request.bind(this, request, response));
+  request.on("error", log.bind(this));
 
-  request.on( 'data', collect_data.bind( this ) );
-  request.on( 'end', process_request.bind( this, request, response ) );
-  request.on( 'error', log.bind( this ) );
-
-  response.on( 'close', log.bind( this, 'done' ) );
-  response.on( 'error', log.bind( this ) );
+  response.on("close", log.bind(this, "done"));
+  response.on("error", log.bind(this));
 
   response.statusCode = 200; // until trouble we assume ok ...
-  response.statusMessage = 'All Good';
-  response.setEncoding = ( 'utf8' );
-  
-  reqURL = new URL('https://' +request.headers.host + '/' +  request.url);
-  log(request.headers['user-agent']);
-  log(request.headers['host']);
-  
-  let params = new URLSearchParams(reqURL['search']);
-  log(params.get('filter'));
-  log(params.get('filterx'));
+  response.statusMessage = "All Good";
+  response.setEncoding = "utf8";
+
+  reqURL = new URL("https://" + request.headers.host + "/" + request.url);
+  log(request.headers["user-agent"]);
+  log(request.headers["host"]);
+
+  let params = new URLSearchParams(reqURL["search"]);
+  log(params.get("filter"));
+  log(params.get("filterx"));
 
   //reqURL = new URL( 'https://localhost:3000/' + request.url );
 
-  if ( reqURL.search.length > 0 ) argGet = reqURL.search; // there are several request per roundtrip. We try to grab the first one
+  if (reqURL.search.length > 0) argGet = reqURL.search; // there are several request per roundtrip. We try to grab the first one
 }
 /*
-* main has just one child and it could be inside - 
-* but we keep good left margin, by using separate functions
-*/
-async function process_request ( request, response ) {
+ * main has just one child and it could be inside -
+ * but we keep good left margin, by using separate functions
+ */
+async function process_request(request, response) {
+  if (pileUp.length > 0) log(Buffer.concat(pileUp).toString()); // end wire - must connect to real storage
 
-  if ( pileUp.length > 0 ) log( Buffer.concat( pileUp ).toString() ); // end wire - must connect to real storage
+  log(`REQUEST: ${request.method}  ${reqURL}`);
 
-  log( `REQUEST: ${request.method}  ${reqURL}` );
-
-  const file = path.join( __dirname, reqURL.pathname );
+  const file = path.join(__dirname, reqURL.pathname);
 
   /* check for domestic file if no exixtence, try cloud
   if not file request, then try various end points
   */
-  if ( file.split( '.' ).length === 2 ) {
-    fs.createReadStream( file )
-      .on( 'error',
-        async ( msg ) => {
+  if (file.split(".").length === 2) {
+    fs.createReadStream(file)
+      .on("error", async (msg) => {
+        log("readfile " + file + msg);
 
-          log( 'readfile ' + file + msg );
+        let cloud = file.split("\\").pop();
+        const storage = new Storage();
+        const S = await storage.bucket("triticumarchives").file(cloud);
+        S.createReadStream()
+          .on("error", (err) => {
+            log("read API " + file + err);
 
-          let cloud = file.split( '\\' ).pop();
-          const storage = new Storage();
-          const S = await storage.bucket( 'triticumarchives' ).file( cloud );
-          S.createReadStream()
-            .on( 'error', ( err ) => {
-
-              log( 'read API ' + file + err );
-
-              response.statusCode = 404;
-              response.end( '<h1>Problems ' + file + '</h1>', 'utf-8' );
-            } )
-            .pipe( response );
-        } )
-      .pipe( response );
-
+            response.statusCode = 404;
+            response.end("<h1>Problems " + file + "</h1>", "utf-8");
+          })
+          .pipe(response);
+      })
+      .pipe(response);
   } else {
-
-    switch ( reqURL.pathname ) {
-
-      case '//SYMBOL': {
-        let slam = await symbolData(); client_response( slam, response );
+    switch (reqURL.pathname) {
+      case "//SYMBOL": {
+        let slam = await symbolData();
+        client_response(slam, response);
         break;
       }
-      case '//IBM/Manuals': {
+      case "//IBM/Manuals": {
         await IBM_Manuals();
         break;
       }
-      case '//tsv/taCorpEthics': {
-        await closed_position( argGet );
+      case "//tsv/taCorpEthics": {
+        await closed_position(argGet);
         break;
       }
       //case '//csv/x3050_CVR': await closed_position(); break;
-      default: response.end( '<h1>Problems ' + reqURL + '</h1>', 'utf-8' );
+      default:
+        response.end("<h1>Problems " + reqURL + "</h1>", "utf-8");
     }
-
   }
-  async function closed_position ( search ) {
+  async function closed_position(search) {
+    const params = new URLSearchParams(search);
 
-    const params = new URLSearchParams( search );
-
-    let file = params.get( "file" );
+    let file = params.get("file");
 
     const storage = new Storage();
-    const S = await storage.bucket( 'triticum' ).file( file );
+    const S = await storage.bucket("triticum").file(file);
 
     S.createReadStream()
-      .on( 'error',
-        ( err ) => {
-          console.log( err );
-          response.statusCode === 404;
-          response.end( '<h1>Problems ' + fileBucket + ' | ' + err + '</h1>', 'utf-8' );
-        } )
-      .pipe( response );
-
+      .on("error", (err) => {
+        console.log(err);
+        response.statusCode === 404;
+        response.end(
+          "<h1>Problems " + fileBucket + " | " + err + "</h1>",
+          "utf-8"
+        );
+      })
+      .pipe(response);
   }
-  async function IBM_Manuals () {
-
+  async function IBM_Manuals() {
     let lines = [];
     const storage = new Storage();
-    const [ fileObjects ] = await storage.bucket( 'triticumarchives' ).getFiles( { prefix: 'IBM' } );
+    const [fileObjects] = await storage
+      .bucket("triticumarchives")
+      .getFiles({ prefix: "IBM" });
 
-
-    for ( let jx = 0; jx < fileObjects.length; jx++ ) {
-      const F = fileObjects[ jx ];
-      lines.push( `${F.name}\t${F.metadata.updated}\t${F.metadata.timeCreated}\t${F.metadata.size}\t${F.metadata.generation}\t${F.metadata.bucket}\r\n` );
+    for (let jx = 0; jx < fileObjects.length; jx++) {
+      const F = fileObjects[jx];
+      lines.push(
+        `${F.name}\t${F.metadata.updated}\t${F.metadata.timeCreated}\t${F.metadata.size}\t${F.metadata.generation}\t${F.metadata.bucket}\r\n`
+      );
     }
 
-    response.setHeader( 'Content-Type', 'application/json' );
-    response.write( lines.join( '' ) );
+    response.setHeader("Content-Type", "application/json");
+    response.write(lines.join(""));
     response.statusCode = 200;
-    response.statusMessage = 'data extracted';
+    response.statusMessage = "data extracted";
     response.end();
-
   }
 }
 
-function collect_data ( streamBytes ) {
-
-  pileUp.push( streamBytes );
+function collect_data(streamBytes) {
+  pileUp.push(streamBytes);
 
   return;
 }
-function client_response ( what, response ) {
-
-  response.setHeader( 'Content-Type', 'application/json' );
-  response.write( JSON.stringify( what, null, 2 ) );
+function client_response(what, response) {
+  response.setHeader("Content-Type", "application/json");
+  response.write(JSON.stringify(what, null, 2));
   response.statusCode = 200;
-  response.statusMessage = 'data extracted';
+  response.statusMessage = "data extracted";
   response.end();
-
 }
-function log ( what ) {
-
-  process.stdout.write( myname + ' ' + new Date().toISOString() + ' ' + JSON.stringify( what ) + '\r\n' );
+function log(what) {
+  process.stdout.write(
+    myname +
+      " " +
+      new Date().toISOString() +
+      " " +
+      JSON.stringify(what) +
+      "\r\n"
+  );
 
   return;
 }
